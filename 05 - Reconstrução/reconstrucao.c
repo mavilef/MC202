146093ba9a1b250870ec16b2ce7416d0eba4a6e6
Marcelo Vilela <marcelo.filho.cps@gmail.com>
2017-09-17 20:07:21 -0300

    Troquei uma variavel global por um ponteiro

--- 05 - Reconstrução/reconstrucao.c
+++ 05 - Reconstrução/reconstrucao.c
@@ -12,14 +12,9 @@ typedef struct treeNode {
 
 } treeNode;
 
-treeNode *rebuildTree(char *preOrder, char *inOrder, int sequenceStart, int sequenceEnd);
+treeNode *rebuildTree(char *preOrder, char *inOrder, int sequenceStart, int sequenceEnd, int **idx);
 void postOrderPrint(treeNode * tree);
 void destroyTree(treeNode * tree);
-//idx é uma variável global que servira para alterar indices durante a recursão
-//do rebuildTree.É global para zerarmos de uma recursão para outra, mas não
-//durante a chamada.
-
-int idx;
 
 int main()
 {
@@ -27,23 +22,31 @@ int main()
   //*preOrderSequence é uma string que armazenará o input de pré-ordem.
   //*InOrderSequence é uma string que armazenará o input de em-ordem.
   //*tree será a raiz da arvore binária e apartir dela estruturaremos a arvore
+  //idx é uma variável que servira para alterar indices durante a recursão
+  //do rebuildTree.É utilizada como ponteiro para zerarmos de uma entrada para outra, mas não
+  //durante as chamadas, ou seja a cada chamada(independente se for esquerda ou direita) idx é sempre acrescido.
+
 
     char *preOrderSequence = malloc(SETSIZE*sizeof(char));
     char *inOrderSequence = malloc(SETSIZE*sizeof(char));
     treeNode *tree = NULL;
+	int *idx = malloc(sizeof(int));
     //leitura dos inputs até o fim do buffer de entrada.
     while (scanf("%s %s", preOrderSequence, inOrderSequence) != EOF) {
-  		idx = 0;
+  		idx[0] = 0;
       //reconstrução da arvore apartir de preOrderSequence e InOrderSequence.
-  		tree = rebuildTree(preOrderSequence, inOrderSequence, 0, strlen(preOrderSequence) - 1);
+  		tree = rebuildTree(preOrderSequence, inOrderSequence, 0, strlen(preOrderSequence) - 1, &idx);
       //imprimir em pós ordem
   		postOrderPrint(tree);
       //destruir a arvore.
   		destroyTree(tree);
-      tree = NULL;
+     	 tree = NULL;
   		printf("\n");
     }
 
+	free(preOrderSequence);
+	free(inOrderSequence);
+	free(idx);
     return 0;
 }
 
@@ -55,7 +58,7 @@ int main()
 //os da direita, estão á direita da chave de em-ordem.
 //Dentro dos filhos á direita e á esquerda aplicamos o mesmo algoritmo,
 //obtendo os outros pais e filhos recursivamente..
-treeNode *rebuildTree(char *preOrder, char *inOrder, int sequenceStart, int sequenceEnd){
+treeNode *rebuildTree(char *preOrder, char *inOrder, int sequenceStart, int sequenceEnd, int **idx){
 
     //checa se estamos ultrapassando os limites da string, caso esteja não coloca
     //nada no nó(NULL).
@@ -65,9 +68,10 @@ treeNode *rebuildTree(char *preOrder, char *inOrder, int sequenceStart, int sequ
     int nextEnd = 0;
     //alocação do novo elemento e seleciona elemento em pré ordem.
     treeNode *newElement = malloc(sizeof(treeNode));
-    newElement->letter = preOrder[idx++];
+    newElement->letter = preOrder[(*idx)[0]];
     newElement->left = NULL;
     newElement->right = NULL;
+	(*idx)[0]++;
 
     //quando a string analisada só possui um elemento.
     if (sequenceStart == sequenceEnd)
@@ -81,8 +85,8 @@ treeNode *rebuildTree(char *preOrder, char *inOrder, int sequenceStart, int sequ
   		    break;
   		}
     //preenche recursivamente os filhos de cada nó.
-    newElement->left = rebuildTree(preOrder, inOrder, sequenceStart, nextEnd - 1);
-    newElement->right = rebuildTree(preOrder, inOrder, nextEnd + 1, sequenceEnd);
+    newElement->left = rebuildTree(preOrder, inOrder, sequenceStart, nextEnd - 1, idx);
+    newElement->right = rebuildTree(preOrder, inOrder, nextEnd + 1, sequenceEnd, idx);
     return newElement;
 
 }
