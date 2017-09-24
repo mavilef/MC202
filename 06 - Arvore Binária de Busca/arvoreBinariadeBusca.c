7b22251a5ab6f427106ed3faa839402669778d4f
Marcelo Vilela <marcelo.filho.cps@gmail.com>
2017-09-23 22:48:41 -0300

    "lab finalizado e comentado, entregue!"

--- 06 - Arvore Binária de Busca/arvoreBinariadeBusca.c
+++ 06 - Arvore Binária de Busca/arvoreBinariadeBusca.c
@@ -1,3 +1,4 @@
+//Marcelo Martins Vilela Filho RA 202619 - Engenharia de Computação
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -10,10 +11,17 @@ typedef struct binaryTreeNode{
 
 } binaryTreeNode;
 
+typedef struct list{
+
+  binaryTreeNode *node;
+  struct list *next;
+
+} list;
+
 typedef struct queue{
-  int cap;
-  int size;
-  binaryTreeNode *vector;
+
+  list *head;
+  list *tail;
 
 } queue;
 
@@ -25,32 +33,42 @@ void maximumElement(binaryTreeNode *root);
 void postOrderTraversal(binaryTreeNode *root);
 void inOrderTraversal(binaryTreeNode *root);
 void preOrderTraversal(binaryTreeNode *root);
+void levelOrderTraversal(binaryTreeNode *root);
 void destroyTree(binaryTreeNode *root);
+void enqueue(queue *Queue, binaryTreeNode *node);
+binaryTreeNode* dequeue(queue *Queue);
 
 int main(){
 
+  //Declarações:
+  //operation lerá a string que corresponde a operação.
+  //root raiz da arvore.
   char operation[50];
   binaryTreeNode *root = NULL;
-  int numberOfnodes = 0;
 
   while(scanf("%s", operation) != EOF){
 
     if(!strcmp(operation, "inserir")){
       int element;
-      scanf("%d", &element);
-      root = insertElementIntheTree(root, element);
+      if(scanf("%d", &element))
+        root = insertElementIntheTree(root, element);
+      else
+        printf("Erro na leitura do elemento(inserção)");
 
     }else if(!strcmp(operation, "excluir")){
       int element;
-      scanf("%d", &element);
-      root = deleteElementIntheTree(root, element);
-
+      if(scanf("%d", &element))
+        root = deleteElementIntheTree(root, element);
+      else
+        printf("Erro na leitura do elemento(excluir)");
 
     }else if(!strcmp(operation, "buscar")){
       int element;
-      scanf("%d", &element);
-      searchElementIntheTree(root, element);
 
+      if(scanf("%d", &element))
+        searchElementIntheTree(root, element);
+      else
+        printf("Erro na leitura do elemento(busca)");
 
     }else if(!strcmp(operation, "minimo")){
       minimumElement(root);
@@ -80,6 +98,12 @@ int main(){
       printf("\n");
 
     }else if(!strcmp(operation, "largura")){
+      if(root)
+        levelOrderTraversal(root);
+      else
+        printf("vazia");
+      printf("\n");
+
 
     }else if(!strcmp(operation, "terminar")){
       destroyTree(root);
@@ -92,8 +116,12 @@ int main(){
   return 0;
 }
 
+//insertElementIntheTree faz a inserção de Elementos na arvore, seguindo
+//a propriedade da arvore binária de busca onde, se o elemento é maior
+// é colocado a direita, se for menor , á esquerda.
 binaryTreeNode* insertElementIntheTree(binaryTreeNode *root, int elementToAdd){
 
+  //primeira inserção na arvore(sem nós)
   if(root == NULL){
     root = malloc(sizeof(binaryTreeNode));
     if(root == NULL){
@@ -105,6 +133,7 @@ binaryTreeNode* insertElementIntheTree(binaryTreeNode *root, int elementToAdd){
       root->right = NULL;
     }
 
+  //demais nós
   }else{
 
     if(elementToAdd < root->data)
@@ -115,6 +144,7 @@ binaryTreeNode* insertElementIntheTree(binaryTreeNode *root, int elementToAdd){
   return root;
 }
 
+//busca de elementos na arvore.
 void searchElementIntheTree(binaryTreeNode *root, int elementToSearch){
   if(root == NULL){
     printf("nao pertence\n");
@@ -136,6 +166,7 @@ void searchElementIntheTree(binaryTreeNode *root, int elementToSearch){
 
 }
 
+//exclusão de elementos na arvore.
 binaryTreeNode* deleteElementIntheTree(binaryTreeNode *root , int elementToDelete){
 
   binaryTreeNode *aux = NULL;
@@ -143,11 +174,14 @@ binaryTreeNode* deleteElementIntheTree(binaryTreeNode *root , int elementToDelet
   if(root == NULL){
     return root;
   }else if(elementToDelete > root->data){
+
+    //encontrar o elemento recursivamente.
     root->right = deleteElementIntheTree(root->right, elementToDelete);
   }else if(elementToDelete < root->data){
+    //encontrar o elemento recusivamente.
     root->left = deleteElementIntheTree(root->left, elementToDelete);
   }else{
-
+    //Nesse ponto encontramos o nó a ser apagado.
     if(root->left == NULL){
       aux = root->right;
       free(root);
@@ -158,10 +192,12 @@ binaryTreeNode* deleteElementIntheTree(binaryTreeNode *root , int elementToDelet
       return aux;
     }
     aux = root->right;
+    //encontra o sucessor do elemento
     while(aux->left != NULL)
       aux = aux->left;
-
+    //substitui o elemento a ser deletado pelo sucessor.
     root->data =  aux->data;
+    //exclui o nó do sucessor.
     root->right = deleteElementIntheTree(root->right, root->data);
 
   }
@@ -170,6 +206,9 @@ binaryTreeNode* deleteElementIntheTree(binaryTreeNode *root , int elementToDelet
 
 }
 
+//minimumElement procura o menor elemento da arvore/subarvore.
+//o menor elemento é sempre o elemento mais a esquerda da arvore,
+//conforme propriedade da arvore binaria de busca.
 void minimumElement(binaryTreeNode *root){
   if(root == NULL){
     printf("vazia\n");
@@ -179,12 +218,17 @@ void minimumElement(binaryTreeNode *root){
     printf("%d\n", root->data);
     return;
   }
+  //chega no elemento mais a esquerda
+  //percorrendo iterativamente.
   while(root->left != NULL)
     root = root->left;
 
   printf("%d\n", root->data);
 }
 
+//maximumElement procura o maior elemento da arvore/subarvore.
+//o maior elemento é sempre o elemento mais a direita da arvore,
+//conforme propriedade da arvore binaria de busca.
 void maximumElement(binaryTreeNode *root){
   if(root == NULL){
     printf("vazia\n");
@@ -194,12 +238,16 @@ void maximumElement(binaryTreeNode *root){
     printf("%d\n", root->data);
     return;
   }
+  //chega no elemento mais a esquerda
+  //percorrendo iterativamente.
   while(root->right != NULL)
     root = root->right;
 
   printf("%d\n", root->data);
 }
 
+//postOrderTraversal percorre a lista recursivamente,
+//de acordo com o método pós-ordem.
 void postOrderTraversal(binaryTreeNode *root){
 
   if(root == NULL)
@@ -210,6 +258,8 @@ void postOrderTraversal(binaryTreeNode *root){
 
 }
 
+//inOrderTraversal percorre a lista recursivamente,
+//de acordo com o método em-ordem.
 void inOrderTraversal(binaryTreeNode *root){
 
   if(root == NULL)
@@ -220,6 +270,8 @@ void inOrderTraversal(binaryTreeNode *root){
 
 }
 
+//preOrderTraversal percorre a lista recursivamente,
+//de acordo com o método pré-ordem.
 void preOrderTraversal(binaryTreeNode *root){
   if(root == NULL)
     return;
@@ -229,27 +281,35 @@ void preOrderTraversal(binaryTreeNode *root){
 
 }
 
+//levelOrderTraversal percorre a lista iterativamente,
+//visitando os nós por nivel.
+//Tal função foi feita iterativamente, utilizando filas,
+//e duas funções da fila: enfileirar(enqueue) e desinfileirar(dequeue);
+//basicamente a fila vai armazenando os nós(endereços) de cada nivel em ordem,
+//desinfileirando, obtendo os filhos de cada nó, após o desinfileiramento.
 void levelOrderTraversal(binaryTreeNode *root){
 
   binaryTreeNode *aux = root;
-  queue levelOrderQueue;
-  levelOrderQueue->cap = 2;
-  levelOrderQueue->size = 1
-  levelOrderQueue->vector = calloc(2*sizeof(binaryTreeNode));
+  //criação da fila.
+  queue *Queue = malloc(sizeof(queue));
+  Queue->head = NULL;
+  Queue->tail = NULL;
 
-  levelOrderQueue->vector[0] = aux; 
-  while(size != 0){
-    aux = levelOrderQueue->vector[0];
+  enqueue(Queue, root);
+
+  while(Queue->head != NULL){
+    aux = dequeue(Queue);
     printf("%d ", aux->data);
-    levelOrderQueue->vector[]
-
-
-
-
+    if(aux->left != NULL)
+      enqueue(Queue, aux->left);
+    if(aux->right != NULL)
+      enqueue(Queue, aux->right);
   }
-
+  free(Queue);
 }
 
+//destroyTree libera a memoria de todos os nós da arvore,
+//segue o pós-ordem porém liberando memoria.
 void destroyTree(binaryTreeNode *root){
 
     if (root == NULL)
@@ -258,5 +318,42 @@ void destroyTree(binaryTreeNode *root){
     destroyTree(root->left);
     destroyTree(root->right);
     free(root);
+
+}
+
+//função de enfileirar.
+void enqueue(queue *Queue, binaryTreeNode *node){
+
+  list *newElement = malloc(sizeof(list));
+  if(newElement == NULL){
+    printf("Erro ao enfileirar\n");
+    return;
+  }
+
+  newElement->node = node;
+  newElement->next = NULL;
+
+  if(Queue->head == NULL)
+    (Queue->tail) = (Queue->head) = newElement;
+  else{
+    (Queue->tail)->next = newElement;
+    (Queue->tail) = (Queue->tail)->next;
+  }
+
+}
+
+//desinfileirar
+binaryTreeNode* dequeue(queue *Queue){
+  if(Queue->head == NULL)
+    return NULL;
+
+  list *aux = NULL;
+  binaryTreeNode *aux2 = NULL;
+  aux2 = (Queue->head)->node;
+  aux = (Queue->head);
+  (Queue->head) = (Queue->head)->next;
+  free(aux);
+
+  return aux2;
 
 }
