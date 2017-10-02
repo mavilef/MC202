c710eb8433e6e7c603c348ff772fc893b18aa1a1
Marcelo Vilela <marcelo.filho.cps@gmail.com>
2017-10-01 23:46:10 -0300

    lab 07 finalizado e comentado

--- 07 - Lca/lca.c
+++ 07 - Lca/lca.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
 //Marcelo Martins Vilela Filho RA 202619 - Engenharia de Computação
+#include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
@@ -10,11 +10,22 @@ typedef struct forestStructure{
 
 forestStructure* createForest(int numberOfTrees);
 
+//Forma de implementação:
+// Foi usada uma implementação de vetores, onde cada elemento do vetor possui
+//a chave(data) e o indice do pai daquele nó.
 int main(){
+	//declarações:
+	// numberOftree será o numero de arvores da nossa floresta, elas são
+	//inicializadas sem filhos.
+	// numberOfOperations indica o numero de operações para realizarmos na floresta,
+	//é a condição de parada na leitura das operações.
+	//operation corresponde a string digitada pelo usuario, que correponde,
+	//à operação desejada.
 
 	int numberOfTrees, numberOfOperations;
 	char operation[5];
 	forestStructure* myForest = NULL;
+	//leitura dos inputs.
 	scanf("%d %d", &numberOfTrees, &numberOfOperations);
 
 	if(!numberOfOperations)
@@ -28,10 +39,13 @@ int main(){
 		if(!strcmp("cut", operation)){
 			int element;
 			scanf("%d", &element);
+			//Faz o elemento selecionado pelo usuario, perder a conecção pai-filho,
+			//o elemento agora será uma outra arvore.
 			myForest[element-1].fatherIndex = -1;
 		}else if(!strcmp("link", operation)){
 			int A, B;
 			scanf("%d %d", &A, &B);
+			//Altera o indice do pai de A(digitado pelo usuario) como sendo B.
 			myForest[A-1].fatherIndex = B;
 		}else if(!strcmp("lca", operation)){
 			int A, B;
@@ -39,6 +53,10 @@ int main(){
 			int aux;
 			int aux2;
 
+			//Através de um método brute force, seleciona B e vai percorrendo os pais
+			//até que encontre A, caso não encontre numa passada, é selecionado o pai de A
+			//e reinicia-se o processo com B, até que se encontre um A == B, como o processo
+			//se inicia do da base temos o menor ancestral.
 			for(aux = A-1;  aux != -1; aux = myForest[aux].fatherIndex - 1){
 				for(aux2 = B-1; aux2 != -1; aux2 = myForest[aux2].fatherIndex - 1){
 					if(aux == aux2){
@@ -55,16 +73,20 @@ int main(){
 			continue;
 		}
 	}
+	free(myForest);
 	return 0;
 }
 
+//CreateForest cria o vetor inicial com todas as arvores, que possuem só raiz.
 forestStructure *createForest(int numberOfTrees){
 
 	forestStructure *initialForest = malloc(numberOfTrees*sizeof(forestStructure));
 
 	int i = 0;
 	for(i = 0; i < numberOfTrees; i++){
+		//chaves de 1 á N
 		initialForest[i].data = i+1;
+		//-1 indica que o nó não possui pai.
 		initialForest[i].fatherIndex = -1;
 	}
 
